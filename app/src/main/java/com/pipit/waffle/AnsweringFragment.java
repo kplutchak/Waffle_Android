59fa0d5ea8c9334bc1fbecd50ccf767b3a3ad5b9
kplutchak <kplutchak@ucla.edu>
2014-11-21 18:05:02 -0800

    Beginning on scrolling animation for images, beginning question creation fragment

--- app/src/main/java/com/pipit/waffle/AnsweringFragment.java
+++ app/src/main/java/com/pipit/waffle/AnsweringFragment.java
@@ -1,10 +1,13 @@
 package com.pipit.waffle;
 
+import android.graphics.Outline;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.Button;
 
 /**
  * Created by Kyle on 11/19/2014.
@@ -15,7 +18,52 @@ public class AnsweringFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.answering_fragment, container, false);
+        View v = inflater.inflate(R.layout.answering_fragment, container, false);
+
+        //Outline(s)
+        Button fab = (Button) v.findViewById(R.id.answer_text);
+
+        ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                // Or read size directly from the view's width/height
+                int size = getResources().getDimensionPixelSize(R.dimen.button_answering_size);
+                outline.setOval(0, 0, size, size);
+            }
+        };
+        fab.setOutlineProvider(viewOutlineProvider);
+
+        fab.setClipToOutline(true);
+
+        Button fab2 = (Button) v.findViewById(R.id.answer_camera);
+
+        ViewOutlineProvider viewOutlineProvider2 = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                // Or read size directly from the view's width/height
+                int size = getResources().getDimensionPixelSize(R.dimen.button_answering_size);
+                outline.setOval(0, 0, size, size);
+            }
+        };
+        fab2.setOutlineProvider(viewOutlineProvider);
+
+        fab2.setClipToOutline(true);
+
+        Button fab3 = (Button) v.findViewById(R.id.answer_voice);
+
+        ViewOutlineProvider viewOutlineProvider3 = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                // Or read size directly from the view's width/height
+                int size = getResources().getDimensionPixelSize(R.dimen.button_answering_size);
+                outline.setOval(0, 0, size, size);
+            }
+        };
+        fab3.setOutlineProvider(viewOutlineProvider);
+
+        fab3.setClipToOutline(true);
+
+        return v;
     }
 
 }
--- app/src/main/java/com/pipit/waffle/ModeSelectionFragment.java
+++ app/src/main/java/com/pipit/waffle/ModeSelectionFragment.java
@@ -3,11 +3,14 @@ package com.pipit.waffle;
 import android.graphics.Outline;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 /**
@@ -19,24 +22,46 @@ public class ModeSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //Outline
+        // Outline
 
         View v = inflater.inflate(R.layout.mode_selection_fragment, container, false);
 
-        ImageView profile = (ImageView) v.findViewById(R.id.profile);
+        CardView cv = (CardView) v.findViewById(R.id.card_view);
 
-        ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
+        // CardView movement
+        View.OnTouchListener tl = new View.OnTouchListener()
+        {
+            public float offsetX;
+            public float offsetY;
+
             @Override
-            public void getOutline(View view, Outline outline) {
-                // Or read size directly from the view's width/height
-                int size = getResources().getDimensionPixelSize(R.dimen.profile_size);
-                outline.setOval(0, 0, size, size);
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                int theAction = event.getAction();
+                switch (theAction)
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        // Button down
+                        offsetX = v.getX() - event.getRawX();
+                        offsetY = v.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        // Button moved
+                        float newX = event.getRawX() + offsetX;
+                        float newY = event.getRawY() + offsetY;
+                        v.setX(newX);
+                        //v.setY(newY);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // Button up
+                        break;
+                    default:
+                        break;
+                }
+                return true;
             }
         };
-        profile.setOutlineProvider(viewOutlineProvider);
-
-        profile.setClipToOutline(true);
-
+        cv.setOnTouchListener(tl);
 
         return v;
     }
